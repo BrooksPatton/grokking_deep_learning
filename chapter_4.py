7d507bac879eb6ce41201b52db8270e25ee0c5e6
Brooks Patton <brooks@brookzerker.com>
2019-07-20 21:00:26 -0600

    gradient descent example

--- chapter_4.py
+++ chapter_4.py
@@ -1,6 +1,5 @@
-weight = 0.5
-input = 0.5
-expected_prediction = 0.8
+weight = 0.1
+learning_speed = 0.01
 
 
 def neural_network(input, weight):
@@ -8,9 +7,14 @@ def neural_network(input, weight):
     return prediction
 
 
-for _ in range(22):
+number_of_toes = [8.5]
+won_game = [1]
+input = number_of_toes[0]
+goal_prediction = won_game[0]
+
+for _ in range(10):
     prediction = neural_network(input, weight)
-    error = (prediction - expected_prediction) ** 2
-    learn_rate = (prediction - expected_prediction) * input
-    weight = weight - learn_rate
-    print("Error: " + str(error) + ' prediction: ' + str(prediction))
+    error = (prediction - goal_prediction) ** 2
+    error_delta = prediction - goal_prediction
+    possible_new_weight = error_delta * input
+    weight = weight - (possible_new_weight * learning_speed)
