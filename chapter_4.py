81d60cd48c82214657f15ee4752f8e34ccee9fa7
Brooks Patton <brooks@brookzerker.com>
2019-07-20 20:37:04 -0600

    gradient descent learning

--- chapter_4.py
+++ chapter_4.py
@@ -1,7 +1,6 @@
 weight = 0.5
 input = 0.5
 expected_prediction = 0.8
-learn_rate = 0.001
 
 
 def neural_network(input, weight):
@@ -9,18 +8,9 @@ def neural_network(input, weight):
     return prediction
 
 
-for _ in range(1103):
+for _ in range(22):
     prediction = neural_network(input, weight)
     error = (prediction - expected_prediction) ** 2
+    learn_rate = (prediction - expected_prediction) * input
+    weight = weight - learn_rate
     print("Error: " + str(error) + ' prediction: ' + str(prediction))
-
-    up_prediction = neural_network(input, weight + learn_rate)
-    up_error = (up_prediction - expected_prediction) ** 2
-    down_prediction = neural_network(input, weight - learn_rate)
-    down_error = (down_prediction - expected_prediction) ** 2
-
-    if(error < up_error or error < down_error):
-        if(up_error < down_error):
-            weight = weight + learn_rate
-        elif(down_error < up_error):
-            weight = weight - learn_rate
