7b301742310900c27dc4fe900154262417bacde4
Brooks Patton <brooks@brookzerker.com>
2019-07-24 19:46:42 -0600

    wip

--- chapter_5.py
+++ chapter_5.py
@@ -1,136 +1,38 @@
-def neural_network(input_data, weights):
-    assert(len(input_data) == len(weights))
-
-    prediction = []
-
-    for weight in weights:
-        prediction.append(weighted_sum(input_data, weight))
-    
-    return prediction
-
 def weighted_sum(vector_1, vector_2):
     assert(len(vector_1) == len(vector_2))
-
     result = 0
-
     for index in range(len(vector_1)):
         result = result + (vector_1[index] * vector_2[index])
-
     return result
 
-def calculate_deltas(prediction, true):
-    assert(len(prediction) == len(true))
-
-    deltas = []
-
-    for index in range(len(prediction)):
-        deltas.append(prediction[index] - true[index])
-
-    return deltas
-
-def calculate_errors(prediction, true):
-    assert(len(prediction) == len(true))
-
-    errors = []
-
-    for index in range(len(prediction)):
-        errors.append((prediction[index] - true[index]) ** 2)
-
-    return errors
-
-def calculate_weighted_deltas(deltas, input_data):
-    assert(len(deltas) == len(input_data))
-
-    weighted_deltas = []
-
-    for item in input_data:
-        weighted_deltas.append(scalar_vector_multiply(item, deltas))
-
-    return weighted_deltas
-
-def scalar_vector_multiply(scalar, vector):
-    result = []
-
-    for item in vector:
-        result.append(scalar * item)
-
-    return result
-
-def update_weights(weights, weighted_deltas, alpha):
-    assert(len(weights) == len(weighted_deltas))
-
-    for weights_y_index in range(len(weights)):
-        assert(len(weights[weights_y_index]) == len(weighted_deltas[weights_y_index]))
-
-        for weights_x_index in range(len(weights[0])):
-            current_weight = weights[weights_y_index][weights_x_index]
-            
-            weights[weights_y_index][weights_x_index] = current_weight - (weighted_deltas[weights_y_index][weights_x_index] * alpha)
-
-
-def testing():
+def test_weighted_sum():
     print('testing weighted_sum')
-    assert(weighted_sum([1, 2, 3], [4, 5, 6]) == 32)
+    vector_1 = [1, 2, 3]
+    vector_2 = [4, 5, 6]
+    expected_result = 32
+    assert(weighted_sum(vector_1, vector_2) == expected_result)
 
-    print('testing neural_network')
-    input_data = [1, 2, 3]
-    weights = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-    expected_outcome = [14, 32, 50]
-    assert(neural_network(input_data, weights) == expected_outcome)
-
-    print('testing calculate_deltas')
-    prediction = [1, 2, 3]
-    true = [4, 5, 6]
-    expected_outcome = [-3, -3, -3]
-    assert(calculate_deltas(prediction, true) == expected_outcome)
-
-    print('testing calculate_errors')
-    prediction = [1, 2, 3]
-    true = [4, 5, 6]
-    expected_outcome = [9, 9, 9]
-    assert(calculate_errors(prediction, true) == expected_outcome)
-
-    print('testing calculate_weighted_deltas')
-    deltas = [1, 2, 3]
-    input_data = [4, 5, 6]
-    expected_outcome = [
-        [4, 8, 12],
-        [5, 10, 15],
-        [6, 12, 18]
-    ]
-    assert(calculate_weighted_deltas(deltas, input_data) == expected_outcome)
-
-    print('testing scalar_vector_multiply')
-    scalar = 2
+def test_vector_matrix_multiply():
+    print('testing vector_matrix_multiply')
     vector = [1, 2, 3]
-    expected_outcome = [2, 4, 6]
-    assert(scalar_vector_multiply(scalar, vector) == expected_outcome)
-
-    print('testing update_weights')
-    weights = [
+    matrix = [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
     ]
-    weighted_deltas = [
-        [9, 8, 7],
-        [6, 5, 4],
-        [3, 2, 1]
-    ]
-    expected_outcome = [
-        [-89, -78, -67],
-        [-56, -45, -34],
-        [-23, -12, -1]
-    ]
-    alpha = 10
-    update_weights(weights, weighted_deltas, alpha)
-    assert(weights == expected_outcome)
+    expected_result = [14, 32, 50]
+    assert(vector_matrix_multiply(vector, matrix) == expected_result)
 
-# testing()
+def vector_matrix_multiply(vector, matrix):
+    assert(len(vector) == len(matrix))
+    results = []
+    for line in matrix:
+        results.append(weighted_sum(vector, line))
+    return results
+
+def neural_network(input, weights):
+    assert(len(input) == len(weights))
+    return vector_matrix_multiply(input, weights)
 
 weights = [
     [0.1, 0.1, -0.3],
@@ -138,26 +40,14 @@ weights = [
     [0.0, 1.3, 0.1]
 ]
 
-toes = [8.5, 9.5, 9.9, 9.0]
-win_loss_record = [0.65, 0.8, 0.8, 0.9]
-number_of_fans = [1.2, 1.3, 0.5, 1.0]
-
-hurt = [0.1, 0.0, 0.0, 0.1]
-win = [1, 1, 0, 1]
-sad = [0.1, 0.0, 0.1, 0.2]
-
 alpha = 0.01
+inputs = [8.5, 0.65, 1.2]
+what_happened = [0.1, 1, 0.1]
 
-input_data = [toes[0], win_loss_record[0], number_of_fans[0]]
-true = [hurt[0], win[0], sad[0]]
+predictions = neural_network(input, weights)
+errors = list(map(lambda a, b: (a - b) ** 2, predictions, what_happened))
+deltas = list(map(lambda a, b: a - b, predictions, what_happened))
 
-for _ in range(5):
-
-    predictions = neural_network(input_data, weights)
-    deltas = calculate_deltas(predictions, true)
-    errors = calculate_errors(predictions, true)
-    weighted_deltas = calculate_weighted_deltas(deltas, input_data)
-
-    update_weights(weights, weighted_deltas, alpha)
-
-    print(true, predictions)
+# tests
+test_weighted_sum()
+test_vector_matrix_multiply()
